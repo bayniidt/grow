233e3168d7b901ae2db5a1c51e1ff04028e961a0
0049003504@znv.com <a331201489>
2021-04-15 10:30:40 +0800

    pro

---
+++ docs/config.js
@@ -0,0 +1,29 @@
+module.exports = {
+    base: '/grow/',
+	title: 'Grow',
+	description: 'Vuepress blog',
+	themeConfig: {
+		// repo: 'https://github.com/xxxxxxx/blog-demo',
+		// 自定义仓库链接文字。
+		// repoLabel: 'My GitHub',
+		// 右上角Nav
+        nav: [
+            {
+                text: 'Learn',
+                link: '/Learn'
+		    },
+            {
+                text: 'Work',
+                link: '/Work'
+            }
+        ],
+        sidebarDepth: 2,
+        logo: './image/logo.jpg'
+	},
+	// 插件
+	plugins: ['vuepress-plugin-smooth-scroll','autobar'], // 让侧边栏显示跳转文章锚点
+	// ico图标
+	head: [
+		['link', { rel: 'shortcut icon', type: "image/x-icon", href: `./favorite.ico` }]
+    ],
+}
